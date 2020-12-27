5f99f79f1395ebf2784945d6df695210b580fd91
Alex Woroschilow <alex.woroschilow@gmail.com>
2020-12-27 21:00:48 +0100

    =fixed settings

--- src/aodstore/plugins/tinymce.py
+++ src/aodstore/plugins/tinymce.py
@@ -16,7 +16,19 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 TINYMCE_DEFAULT_CONFIG = {
-    "content_css": 'https://materializecss.com/css/ghpages-materialize.css',
+    "content_css": 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css,https://materializecss.com/css/ghpages-materialize.css',
+    "height": "320px",
+    "width": "960px",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+               "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+               "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+               "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+               "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+               "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+
     "formats": {
         "h1": {"block": 'h1', "classes": 'light brown-text text-darken-3'},
         "h2": {"block": 'h2', "classes": 'light brown-text text-darken-3'},
@@ -24,5 +36,6 @@ TINYMCE_DEFAULT_CONFIG = {
         "h4": {"block": 'h4', "classes": 'light brown-text text-darken-3'},
         "h5": {"block": 'h5', "classes": 'light brown-text text-darken-3'},
         "h6": {"block": 'h6', "classes": 'light brown-text text-darken-3'},
+        "p":  { "block": 'p', "classes": 'light brown-text text-darken-3'},
     },
 }
