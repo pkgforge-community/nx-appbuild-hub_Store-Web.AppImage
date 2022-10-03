bc98ea632ead1918bc67bf9ec1253bdc16343d4f
AlexWoroschilow <alex.woroschilow@gmail.com>
2022-10-04 00:09:22 +0200

    =added group collection interface

---
+++ src/frontend/screens/src/GroupCollectionScreen/main.js
@@ -0,0 +1,38 @@
+// Copyright 2021 Alex Woroschilow(alex@ergofox.me)
+//
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+import React from "react";
+import ReactDOM from "react-dom";
+import GroupCollectionScreen from "../components/GroupCollectionScreen";
+
+let container = document.getElementById("GroupCollectionScreen");
+
+if (!container.hasAttribute("data-source")) {
+  throw "Please set [data-source=....]";
+}
+
+if (!container.hasAttribute("data-source-preview")) {
+  throw "Please set [data-source-preview=....]";
+}
+
+if (!container.hasAttribute("data-source-workout")) {
+  throw "Please set [data-source-workout=....]";
+}
+
+const root = ReactDOM.createRoot(container);
+root.render(
+  <GroupCollectionScreen
+    sourceUrl={container.getAttribute("data-source")}
+    sourceUrlPreview={container.getAttribute("data-source-preview")}
+    sourceUrlWorkout={container.getAttribute("data-source-workout")}
+    sourceUrlFavorite={container.getAttribute("data-source-favorite")}
+  />
+);
---
+++ src/frontend/screens/src/components/Group/GroupCollectionElement.jsx
@@ -0,0 +1,44 @@
+// Copyright 2021 Alex Woroschilow(alex@ergofox.me)
+//
+// Licensed under the Apache License, Version 2.0(the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+import React, { useState } from 'react';
+import {
+    Block, Columns, Heading, Image
+} from 'react-bulma-components';
+
+
+export const GroupCollectionElement = (props) => {
+    const [entity, setEntity] = useState(props.entity)
+
+    return (
+        <Block style={{ margin: "10px" }}>
+            <Columns style={{
+                borderRadius: "15px",
+                border: "2px solid #E9E9E9",
+                height: "130px",
+            }}>
+                <Columns.Column size={12} centered={true} align="center">
+                    <Image onClick={() => { console.info(entity) }}
+                        src={entity.icon}
+                        alt={entity.name}
+                        size={48}
+                    />
+                </Columns.Column>
+                <Columns.Column size={12}>
+                    <Heading subtitle weight={"light"} size={5}
+                        onClick={() => { console.info(entity) }}>
+                        {entity.name}
+                    </Heading>
+                </Columns.Column>
+            </Columns>
+        </Block>
+    );
+};
