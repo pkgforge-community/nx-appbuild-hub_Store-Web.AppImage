add11c8b25c9e59c9fde6e3c26c3abbedb9925ff
AlexWoroschilow <alex.woroschilow@gmail.com>
2022-10-04 22:43:03 +0200

    =added new endpoints

--- src/apps/package/rest/group.py
+++ src/apps/package/rest/group.py
@@ -16,10 +16,41 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import serializers
+from django.urls import reverse
 
 from apps.package.model.group import PackageGroup
-from .serializer.group import PackageGroupSerializer
 from drf_yasg.utils import swagger_auto_schema
+
+from .serializer.group import PackageGroupSerializer
+from .serializer.package import PackageSerializer
+
+
+class PackageGroupDetailSerializer(serializers.HyperlinkedModelSerializer):
+    unique = serializers.IntegerField(source='pk')
+    page = serializers.SerializerMethodField()
+
+    packages = PackageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = PackageGroup
+        fields = [
+            'name',
+            'description',
+            'page',
+            'icon',
+            'unique',
+            'packages',
+        ]
+
+    def get_page(self, obj):
+        if 'request' not in self.context.keys():
+            return None
+
+        request = self.context.get('request')
+        return request.build_absolute_uri(
+            reverse('package_group', args=(obj.pk,))
+        )
 
 
 class PackageGroupList(viewsets.GenericViewSet):
@@ -27,6 +58,7 @@ class PackageGroupList(viewsets.GenericViewSet):
     queryset = PackageGroup.objects
     permission_classes = []
     pagination_class = None
+    lookup_url_kwarg = "unique"
 
     def get_queryset(self, search=None):
         if search is not None and len(search):
@@ -34,6 +66,17 @@ class PackageGroupList(viewsets.GenericViewSet):
                 filter(name=search)
 
         return PackageGroup.objects.all()
+
+    @swagger_auto_schema(tags=['API - package-manager'])
+    def retrieve(self, request, *args, **kwargs):
+        unique = self.kwargs.get('unique')
+        if not unique: raise Exception('Unique can not be empty')
+
+        entity = self.queryset.get(pk=unique)
+        if not entity: raise Exception('Group can not be empty')
+
+        serializer = PackageGroupDetailSerializer(entity)
+        return Response(serializer.data)
 
     @swagger_auto_schema(tags=['API - package-manager'])
     def list(self, request, *args, **kwargs):
