4722a8aa8708383c1d961aec5dd989802f828b83
AlexWoroschilow <alex.woroschilow@gmail.com>
2022-10-17 22:01:30 +0200

    =bugfix

--- src/frontend/screens/src/components/Package/PackageCollectionElement.jsx
+++ src/frontend/screens/src/components/Package/PackageCollectionElement.jsx
@@ -9,16 +9,16 @@
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+import Moment from 'moment';
 import React, { useState } from 'react';
 import {
     Block,
-    Columns, Heading, Content
+    Columns, Content, Heading
 } from 'react-bulma-components';
-
-
 import { BiDownload, BiHome } from 'react-icons/bi';
 import { BsGithub } from 'react-icons/bs';
-import Moment from 'moment';
+
+
 
 export const PackageCollectionElement = (props) => {
     const [entity, setEntity] = useState(props.entity)
